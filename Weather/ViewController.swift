715da7473b18208e4b5804c8b9afed85256f5f11
hjg1028 <hjg1028@163.com>
2015-11-15 13:02:49 +0800

    bug fixed

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
@@ -14,34 +15,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var tv: UITextView!
     
     @IBAction func buttonClicked(sender: AnyObject!) {
-        loadWeatherData()
+         try! loadWeatherData()
     }
     
-    func loadWeatherData() {
+    func loadWeatherData() throws {
         
-        let url = NSURL(string: "http://www.weather.com.cn/data/cityinfo/101010100.html")
-        let data = NSData(contentsOfURL: url!)
+        let url = NSURL(string: "http://www.weather.com.cn/data/cityinfo/101010100.html")!
+        let data: NSData = NSData(contentsOfURL: url)!
         
         
-        let json = try! NSJSONSerialization.JSONObjectWithData(data!, options: [])
-//        else {
-//            throw NSError(domain: NSURLErrorDomain, code: NSURLErrorCannotOpenFile, userInfo: nil)
-//        }
+        guard let json = try? NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments)
+            else {
+            throw NSError(domain: NSURLErrorDomain, code: NSURLErrorCannotOpenFile, userInfo: nil)
+        }
         
         let weatherInfo = json.objectForKey("weatherinfo")!
+        
+        //{"weatherinfo":{"city":"北京","cityid":"101010100","temp1":"15℃","temp2":"5℃","weather":"多云","img1":"d1.gif","img2":"n1.gif","ptime":"08:00"}}
+        
         let city = weatherInfo.objectForKey("city")!
-        let weather = weatherInfo.objectForKey("weather")
+        let cityID = weatherInfo.objectForKey("cityid")!
         let temp1 = weatherInfo.objectForKey("temp1")!
         let temp2 = weatherInfo.objectForKey("temp2")!
-        let time = weatherInfo.objectForKey("ptime")!
+        let weather = weatherInfo.objectForKey("weather")!
+        let ptime = weatherInfo.objectForKey("ptime")!
         
-        tv.text = "City: \(city)\nWeather: \(weather)\nTemperature: \(temp2)-\(temp1)\nUpdate time: \(time)"
+        tv.text = "\(city)\n\(temp2)-\(temp1)\n\(weather)\n\(ptime)\n\(cityID)"
         
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadWeatherData()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
